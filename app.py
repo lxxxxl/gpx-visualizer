3e373adcf1b3cfb38d321e3b2b01e359145a026d
lxxxxl <lxxxxl>
2020-05-22 22:06:49 +0300

    file upload functionality

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import logging
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'gpx'}
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            data = file.read()
+            logging.info('%s', data)
+            return ''
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+logging.basicConfig(level=logging.DEBUG)
